aa316a139daabb071f407dd125f30ae50863339e
Eugene Volynko <volynko.ua@gmail.com>
2021-10-28 02:13:09 +0300

    add AnimBackground component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/header';
 import PriceList from './components/price-list';
 import Contacts from './components/contacts';
+import AnimBackground from './components/anim-background';
 
 function App() {
   return (
     <div className="app">
+      {/* <AnimBackground /> */}
       <Header />
       <PriceList />
       <Contacts />
---
+++ src/components/anim-background/index.js
@@ -0,0 +1,15 @@
+import './index.scss';
+
+function AnimBackground() {
+  return (
+    <div className="anim-background">
+      <div className="anim-background__lines">
+        <div className="anim-background__line"></div>
+        <div className="anim-background__line"></div>
+        <div className="anim-background__line"></div>
+      </div>
+    </div>
+  );
+}
+
+export default AnimBackground;
